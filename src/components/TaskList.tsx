35699a22081c1f81c000ecf51ebeffbc32ae08a4
Gabriel Martins Rennó <gabrielmrenno@gmail.com>
2021-10-07 08:13:40 -0300

    :tada: Challenge Finalized

--- src/components/TaskList.tsx
+++ src/components/TaskList.tsx
@@ -16,14 +16,36 @@ export function TaskList() {
 
   function handleCreateNewTask() {
     // Crie uma nova task com um id random, não permita criar caso o título seja vazio.
+    // criar uma newTasks com a newTask e depois atribuir ao estado
+    if(!newTaskTitle) return;
+    const newTask = {
+      id : Math.random(),
+      title: newTaskTitle,
+      isComplete: false
+    }
+
+    setTasks(oldState => [...oldState, newTask]);
+    setNewTaskTitle('');
   }
 
   function handleToggleTaskCompletion(id: number) {
     // Altere entre `true` ou `false` o campo `isComplete` de uma task com dado ID
+    
+    const newTasks = tasks.map(task => task.id === id ? {
+      ...task,
+      isComplete: !task.isComplete
+    } : task)
+
+    setTasks(newTasks);
   }
 
   function handleRemoveTask(id: number) {
     // Remova uma task da listagem pelo ID
+
+    //Retorna as tasks filtrando os ids que são diferente do id passado como parâmetro
+    const filteredTasks = tasks.filter(task => task.id !== id);
+
+    setTasks(filteredTasks);
   }
 
   return (
